765b7505a719595967aed474c6b0a3f5781aecd7
jia-kevin <jia.kevin42@gmail.com>
2017-09-16 16:52:12 -0400

    Beautify

--- StudyBuddyLambda.js
+++ StudyBuddyLambda.js
@@ -1,81 +1,80 @@
 /**
-* Study Buddy
-* Alexa skill that quizzes you and gives live feedback, emplying adaptive learning
-*
-* Hack the North 2017
-* Kevin Jia, Judy Liu, Kevin Zhang
-**/
-
+ * Study Buddy
+ * Alexa skill that quizzes you and gives live feedback, emplying adaptive learning
+ *
+ * Hack the North 2017
+ * Kevin Jia, Judy Liu, Kevin Zhang
+ **/
 'use strict';
 
 // --------------- Helpers that build all of the responses -----------------------
 
 function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-  return {
-    outputSpeech: {
-      type: 'PlainText',
-      text: output,
-    },
-    card: {
-      type: 'Simple',
-      title: `SessionSpeechlet - ${title}`,
-      content: `SessionSpeechlet - ${output}`,
-    },
-    reprompt: {
-      outputSpeech: {
-        type: 'PlainText',
-        text: repromptText,
-      },
-    },
-    shouldEndSession,
-  };
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
 }
 
 function buildResponse(sessionAttributes, speechletResponse) {
-  return {
-    version: '1.0',
-    sessionAttributes,
-    response: speechletResponse,
-  };
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
 }
 
 
 // --------------- Functions that control the skill's behavior -----------------------
 
 function getWelcomeResponse(callback) {
-  // If we wanted to initialize the session to have some attributes we could add those here.
-  const sessionAttributes = {};
-  const cardTitle = 'Welcome';
-  const speechOutput = 'Welcome to the Study Buddy' +
-  'Please pick a category that you would wish to study from.';
-  // If the user either does not reply to the welcome message or says something that is not
-  // understood, they will be prompted again with this text.
-  const repromptText = 'Please pick a category'
-  const shouldEndSession = false;
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Welcome to the Study Buddy' +
+        'Please pick a category that you would wish to study from.';
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Please pick a category'
+    const shouldEndSession = false;
 
-  callback(sessionAttributes,
-    buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-  }
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
 
-  function handleSessionEndRequest(callback) {
+function handleSessionEndRequest(callback) {
     const cardTitle = 'Session Ended';
     const speechOutput = 'Thank you for trying the Alexa Skills Kit sample. Have a nice day!';
     // Setting this to true ends the session and exits the skill.
     const shouldEndSession = true;
 
     callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
-  }
+}
 
-  function createFavoriteColorAttributes(favoriteColor) {
+function createFavoriteColorAttributes(favoriteColor) {
     return {
-      favoriteColor,
+        favoriteColor,
     };
-  }
+}
 
-  /**
-  * Sets the color in the session and prepares the speech to reply to the user.
-  */
-  function setColorInSession(intent, session, callback) {
+/**
+ * Sets the color in the session and prepares the speech to reply to the user.
+ */
+function setColorInSession(intent, session, callback) {
     const cardTitle = intent.name;
     const favoriteColorSlot = intent.slots.Color;
     let repromptText = '';
@@ -84,145 +83,147 @@ function getWelcomeResponse(callback) {
     let speechOutput = '';
 
     if (favoriteColorSlot) {
-      const favoriteColor = favoriteColorSlot.value;
-      sessionAttributes = createFavoriteColorAttributes(favoriteColor);
-      speechOutput = `I now know your favorite color is ${favoriteColor}. You can ask me ` +
-      "your favorite color by saying, what's my favorite color?";
-      repromptText = "You can ask me your favorite color by saying, what's my favorite color?";
+        const favoriteColor = favoriteColorSlot.value;
+        sessionAttributes = createFavoriteColorAttributes(favoriteColor);
+        speechOutput = `I now know your favorite color is ${favoriteColor}. You can ask me ` +
+            "your favorite color by saying, what's my favorite color?";
+        repromptText = "You can ask me your favorite color by saying, what's my favorite color?";
     } else {
-      speechOutput = "I'm not sure what your favorite color is. Please try again.";
-      repromptText = "I'm not sure what your favorite color is. You can tell me your " +
-      'favorite color by saying, my favorite color is red';
+        speechOutput = "I'm not sure what your favorite color is. Please try again.";
+        repromptText = "I'm not sure what your favorite color is. You can tell me your " +
+            'favorite color by saying, my favorite color is red';
     }
 
     callback(sessionAttributes,
-      buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function getColorFromSession(intent, session, callback) {
+    let favoriteColor;
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = false;
+    let speechOutput = '';
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
     }
 
-    function getColorFromSession(intent, session, callback) {
-      let favoriteColor;
-      const repromptText = null;
-      const sessionAttributes = {};
-      let shouldEndSession = false;
-      let speechOutput = '';
-
-      if (session.attributes) {
-        favoriteColor = session.attributes.favoriteColor;
-      }
-
-      if (favoriteColor) {
+    if (favoriteColor) {
         speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
         shouldEndSession = true;
-      } else {
+    } else {
         speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
-        ' is red';
-      }
+            ' is red';
+    }
 
-      // Setting repromptText to null signifies that we do not want to reprompt the user.
-      // If the user does not respond or says something that is not understood, the session
-      // will end.
-      callback(sessionAttributes,
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
         buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-      }
+}
 
 
-      // --------------- Events -----------------------
+// --------------- Events -----------------------
 
-      /**
-      * Called when the session starts.
-      */
-      function onSessionStarted(sessionStartedRequest, session) {
-        console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
-      }
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
 
-      /**
-      * Called when the user launches the skill without specifying what they want.
-      */
-      function onLaunch(launchRequest, session, callback) {
-        console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
 
-        // Dispatch to your skill's launch.
-        getWelcomeResponse(callback);
-      }
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
 
-      /**
-      * Called when the user specifies an intent for this skill.
-      */
-      function onIntent(intentRequest, session, callback) {
-        console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
 
-        const intent = intentRequest.intent;
-        const intentName = intentRequest.intent.name;
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
 
-        // Dispatch to your skill's intent handlers
-        if (intentName === 'categorySelect') {
-          categorySelect(intent, session, callback);
-        } else if (intentName === 'quizSelect') {
-          quizSelect(intent, session, callback);
-        } else if (intentName === 'nextQuestion') {
-          nextQuestion(intent, session, callback);
-        } else if (intentName === 'repeatQuestion') {
-          repeatQuestion(intent, session, callback);
-        } else if (intentName === 'skipQuestion') {
-          skipQuestion(intent, session, callback);
-        } else if (intentName === 'endQuiz') {
-          endQuiz(intent, session, callback);
-        } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
-          handleSessionEndRequest(callback);
-        } else {
-          throw new Error('Invalid intent');
-        }
-      }
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'categorySelect') {
+        categorySelect(intent, session, callback);
+    } else if (intentName === 'quizSelect') {
+        quizSelect(intent, session, callback);
+    } else if (intentName === 'nextQuestion') {
+        nextQuestion(intent, session, callback);
+    } else if (intentName === 'repeatQuestion') {
+        repeatQuestion(intent, session, callback);
+    } else if (intentName === 'skipQuestion') {
+        skipQuestion(intent, session, callback);
+    } else if (intentName === 'endQuiz') {
+        endQuiz(intent, session, callback);
+    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        handleSessionEndRequest(callback);
+    } else {
+        throw new Error('Invalid intent');
+    }
+}
 
-      /**
-      * Called when the user ends the session.
-      * Is not called when the skill returns shouldEndSession=true.
-      */
-      function onSessionEnded(sessionEndedRequest, session) {
-        console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
-        // Add cleanup logic here
-      }
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+    // Add cleanup logic here
+}
 
 
-      // --------------- Main handler -----------------------
+// --------------- Main handler -----------------------
 
-      // Route the incoming request based on type (LaunchRequest, IntentRequest,
-      // etc.) The JSON body of the request is provided in the event parameter.
-      exports.handler = (event, context, callback) => {
-        try {
-          console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
 
-          /**
-          * Uncomment this if statement and populate with your skill's application ID to
-          * prevent someone else from configuring a skill that sends requests to this function.
-          */
-          /*
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
           if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
           callback('Invalid Application ID');
         }
         */
 
         if (event.session.new) {
-          onSessionStarted({ requestId: event.request.requestId }, event.session);
+            onSessionStarted({
+                requestId: event.request.requestId
+            }, event.session);
         }
 
         if (event.request.type === 'LaunchRequest') {
-          onLaunch(event.request,
-            event.session,
-            (sessionAttributes, speechletResponse) => {
-              callback(null, buildResponse(sessionAttributes, speechletResponse));
-            });
-          } else if (event.request.type === 'IntentRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
             onIntent(event.request,
-              event.session,
-              (sessionAttributes, speechletResponse) => {
-                callback(null, buildResponse(sessionAttributes, speechletResponse));
-              });
-            } else if (event.request.type === 'SessionEndedRequest') {
-              onSessionEnded(event.request, event.session);
-              callback();
-            }
-          } catch (err) {
-            callback(err);
-          }
-        };
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
+    }
+};
